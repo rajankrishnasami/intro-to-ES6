d0f5182ff299a98afc5e9f94ae3312ca581297a6
rajankrishnasami <102790130+rajankrishnasami@users.noreply.github.com>
2022-04-09 18:38:21 +0530

    Create template literals.js

---
+++ template literals.js
@@ -0,0 +1,36 @@
+// Past JavaScript
+
+    let word1 = 'Rajan';
+    let word2 = ' .K';
+    const fullName1 = word1 + ' ' + word2;
+    console.log(fullName1);
+
+// Addition of strings using Template Literals
+
+    let word3 = 'Rajan';
+    let word4 = ' Krishnasami';
+    const fullName2 = `${word3} ${word4}`;
+    console.log(fullName2);
+    console.log('')
+
+//  Addition of two numbers using Template Literals
+
+    let num1 = 7;
+    let num2 = 11;
+    const number = `${num1+num2}`;
+    console.log(number);
+    console.log('')
+
+// In the past Multiline string
+
+let example1 = 'Hello \n' + 'world';
+console.log(example1);
+console.log('')
+
+// Multiline string using Template Literal
+
+let example2 = `${'Hello'}
+${'World'}
+`;
+console.log(example2);
+console.log('')
